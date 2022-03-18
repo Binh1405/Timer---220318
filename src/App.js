618421fc8d71cc59d89e0093a5460f1587928bb5
binh1405 <binhhoangngoc@gmail.com>
2022-03-18 19:04:15 +0700

    demo timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [second, setSecond] = useState("00");
+  const [minute, setMinute] = useState("00");
+  const [hour, setHour] = useState("00");
+  const [error, setError] = useState("");
+  let seconds = 0;
+  const timerId = useRef();
+
+  const timer = () => {
+    seconds++;
+    setHour(Math.floor(seconds / 3600));
+    setMinute(Math.floor((seconds - hour * 3600) / 60));
+    setSecond(seconds % 60);
+  };
+
+  const handleStart = () => {
+    if (timerId.current) throw new Error("");
+    timerId.current = setInterval(timer, 1000);
+    console.log("start", timerId.current);
+  };
+  const handleStop = () => {
+    clearInterval(timerId.current);
+    console.log("stop", timerId.current);
+  };
+  const handleReset = () => {
+    handleStop();
+    seconds = 0;
+    setMinute("00");
+    setSecond("00");
+    setHour("00");
+  };
+  // console.log(`0${second}`);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App container">
+        <h1>Coder Timer</h1>
+        <div className="watch">
+          <div className="time">
+            <p>
+              {`0${hour}`.slice(-2)}:{`0${minute}`.slice(-2)}:
+              {`0${second}`.slice(-2)}
+            </p>
+          </div>
+          <div className="errorMessage">{error}</div>
+          <div className="controller">
+            <button className="start" onClick={handleStart}>
+              START
+            </button>
+            <button className="stop" onClick={handleStop}>
+              STOP
+            </button>
+            <button className="reset" onClick={handleReset}>
+              RESET
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
